55a2f23dbc9f4a2b85287907ce1b0c4b95163bad
DongariSai <48290076+DongariSai@users.noreply.github.com>
2019-03-06 21:40:37 +0530

    EvenOrOdd
    ->Read N value 
    ->Check whether given number is Even or Odd

---
+++ EvenOrOddProgram.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+    int n;
+    printf("Enter n");
+    scanf("%d",&n);
+    if(n%2==0){
+     printf("%d=is even number",n);
+    }
+    else
+    {
+     printf("%d=is not even number",n);
+     }
+}
+  
+
+		  
